0292f150264bce2e62f71dabe3f98e7b8c082eb5
thomasfjordside <thomas@thomasfjordside.com>
2022-11-16 20:52:56 +0100

    testing

---
+++ components/BlockText.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="c-block-text w-6col mx-auto my-lg">
+		<div
+			class="c-block-text__content text-body text-gray-700"
+			v-html="item.text"
+		></div>
+	</div>
+</template>
+
+<script setup>
+defineProps({
+	item: Object,
+});
+</script>
